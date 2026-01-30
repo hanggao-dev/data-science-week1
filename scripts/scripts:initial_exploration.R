3faa4b8f8ea5cc0e051087d90f772ce17b35bee2
Hang Gao <H12138G@outlook.com>
2026-01-30 14:34:00 +0000

    Add dataset and initial exploration script

---
+++ scripts/scripts:initial_exploration.R
@@ -0,0 +1,44 @@
+#Initial exploration script
+# Week 1: Initial Data Exploration ====
+# Author: [Your Name]
+# Date: [Today's Date]
+
+# Load packages ====
+library(tidyverse)
+library(here)
+library(naniar)
+library(janitor)
+library(skimr)
+
+# Load data ====
+mosquito_egg_raw <- read_csv(here("data", "mosquito_egg_data.csv"),
+                             name_repair = janitor::make_clean_names)
+
+# Basic overview ====
+glimpse(mosquito_egg_raw)
+summary(mosquito_egg_raw)
+skim(mosquito_egg_raw)
+
+# React table====
+# view interactive table of data
+view(mosquito_egg_raw)
+
+
+# Counts by site and treatment====
+
+mosquito_egg_raw |> 
+  group_by(site, treatment) |> 
+  summarise(n = n())
+
+# Observations ====
+# Your observations (add as comments below):
+# - What biological system is this?
+#   Mosquito
+# - What's being measured?
+#   Mosquito egg count
+# - How many observations?
+#   205 observations and 9 variable
+# - Anything surprising?
+#   Collector is missing
+# - Any obvious problems?
+# - Collector is missing
