5ce266fe2059761039636f329b4223d346cfc970
Hang Gao <H12138G@outlook.com>
2026-02-06 14:15:40 +0000

    Add cleaning steps to raw data.

--- scripts/scripts:initial_exploration.R
+++ scripts/scripts:initial_exploration.R
@@ -43,4 +43,41 @@ mosquito_egg_raw |>
 # - Any obvious problems?
 # - Collector is missing
 
-next go over the tutorial think about it and then pick 2 chapter that you owuld use to apply into your own dataset. that is what you need to do before next lesson.
+#FIX 1: [Fixing negative values of body mass]
+#Show the problem: mosquito egg has a negative body mass
+mosquito_egg_raw |>
+  filter(body_mass_mg <0)
+
+#Fixing 1
+mosquito_egg_clean <- mosquito_egg_raw |>
+  mutate(body_mass_mg = if_else(body_mass_mg<0, NA_real_, body_mass_mg))
+  
+#Verify
+mosquito_egg_clean |>
+  filter(body_mass_mg <0)
+  
+# What changed and why it matters:
+#I changed the negative body mass to NA to avoid error during statistical analysis.
+
+#Fixing 2
+
+# Show the problem: treatment has inconsistent capital letters
+mosquito_egg_clean |>
+  count(treatment)
+
+#Fix it
+mosquito_egg_string <- mosquito_egg_clean |>
+  mutate(treatment = str_to_lower(treatment))
+  
+
+#Verify
+mosquito_egg_string|>
+  count(treatment)
+
+#What changed and why it matters:
+#Using the mutate function I change all the string to a lower case
+#Avoid error during analysis
+#Originally it has a 12 rows of overlapping variables now it has been shortened to three distinct variable
+
+
+
